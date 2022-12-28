77286feae38db66636b8979bd81fec8ad28668db
Artem Morozov <deadtems@yandex.ru>
2022-12-28 15:37:53 +0500

    feat: Add mockapi

--- src/App.js
+++ src/App.js
@@ -4,11 +4,23 @@ import Categories from './components/Categories';
 import Sort from './components/Sort';
 import RollBlock from './components/RollBlock';
 
-import rolls from './assets/rolls.json';
+// import rolls from './assets/rolls.json';
 
 import './scss/app.scss';
 
 function App() {
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('https://63aafafbfdc006ba6051e61a.mockapi.io/items')
+      .then((res) => {
+        return res.json();
+      })
+      .then((arr) => {
+        setItems(arr);
+      });
+  }, []);
+
   return (
     <div className='wrapper'>
       <Header />
@@ -20,7 +32,7 @@ function App() {
           </div>
           <h2 className='content__title'>Все роллы</h2>
           <div className='content__items'>
-            {rolls.map((rolls) => (
+            {items.map((rolls) => (
               <RollBlock key={rolls.id} {...rolls} />
             ))}
           </div>
